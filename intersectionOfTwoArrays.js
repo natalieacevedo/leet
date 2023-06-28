b4c4538f9733dd9909207569a9e7a97b9cea3fba
Natalie Acevedo <natalieacevedo25@gmail.com>
2023-06-28 11:52:33 +0100

    Solution for loops

--- intersectionOfTwoArrays.js
+++ intersectionOfTwoArrays.js
@@ -11,19 +11,37 @@ var intersection = function (nums1, nums2) {
 console.log(intersection([1, 2, 2, 1], [2, 2]));
 
 //F:
-var intersect = function (nums1, nums2) {
-  const intersection = [];
-  const used = new Map();
+// var intersect = function (nums1, nums2) {
+//   const intersection = [];
+//   const used = new Map();
+//   for (let i = 0; i < nums1.length; i++) {
+//     for (let j = 0; j < nums2.length; j++) {
+//       const n1 = nums1[i];
+//       const n2 = nums2[j];
+//       if (n1 === n2 && !used.get(j)) {
+//         used.set(j, true);
+//         intersection.push(n1);
+//         break;
+//       }
+//     }
+//   }
+//   console.log(used);
+//   return intersection;
+//};
+function intersect(nums1, nums2) {
+  let finalArr = [];
+  const myMap = new Map();
   for (let i = 0; i < nums1.length; i++) {
     for (let j = 0; j < nums2.length; j++) {
-      const n1 = nums1[i];
-      const n2 = nums2[j];
-      if (n1 === n2 && !used.get(j)) {
-        used.set(j, true);
-        intersection.push(n1);
+      const a = nums1[i];
+      const b = nums2[j];
+      if (a === b && !myMap.get(j)) {
+        myMap.set(j, true);
+        finalArr.push(a);
         break;
       }
     }
   }
-  return intersection;
-};
+  return finalArr;
+}
+console.log(intersect([1, 2, 2, 1], [2, 2]));
---
+++ twoSum.js
@@ -0,0 +1,20 @@
+// Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+// You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+// You can return the answer in any order.
+
+function twoSum(nums, target) {
+  for (let i = 0; i < nums.length; i++) {
+    const n = nums[i];
+    for (let j = 0; j < nums.length; j++) {
+      if (j === i) continue;
+      const m = nums[j];
+      if (n + m === target) {
+        return [i, j];
+      }
+    }
+  }
+}
+
+console.log(twoSum([2, 7, 11, 15], 9));
